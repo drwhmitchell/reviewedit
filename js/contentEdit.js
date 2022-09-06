7caab9303304a9bd3d67ac9f9b1f0369794f9de7
Bill <bill@silvernovus.com>
2022-09-06 09:56:43 -0700

    trying to fix cards

--- js/contentEdit.js
+++ js/contentEdit.js
@@ -147,9 +147,9 @@ async function FillAssetGallery() {
 }
 
 function ShowAssetImages(assetDataArr) {
-  var buf = "<div class='card-group m-1'>";
+  var buf = "<div class='card-group'>";
   for (i=0; i<assetDataArr.length; i++) {
-    buf += "<div class='card text-center' style='width: 18rem;'>";
+    buf += "<div class='card text-center mx-1' style='width: 18rem;'>";
     buf += "<img src='" + assetDataArr[i].url + "' width='200' height='200'>";  
     buf += "<div class='card-body'>";  
     buf += "<h5 class='card-title'>" + assetDataArr[i].keywords + "</h5>";
